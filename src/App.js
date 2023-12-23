e39279e62405f12c61c868329b96fa425f6598e2
Evan Losh <134793412+EvanLosh@users.noreply.github.com>
2023-12-23 14:36:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+// Copilot comments:
+// Certainly! Below is an example of how you can set up routing between the components NewGame, GameBoard, HowToPlay, and Pools using React Router. Make sure you have React Router installed in your project (you can follow the installation instructions from the official documentation).
+// In this example:
+// The <Router> component wraps the entire app.
+// The navigation links (<Link>) allow users to switch between different components.
+// The <Switch> component ensures that only one route is rendered at a time.
+// Each <Route> specifies a path and the corresponding component to render.
+// Remember to create the actual NewGame, GameBoard, HowToPlay, and Pools components in separate files. Happy routing! 🚀🔗
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import NewGame from './components/NewGame';
+import GameBoard from './components/GameBoard';
+import HowToPlay from './components/HowToPlay';
+import Pools from './components/Pools';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">New Game</Link>
+            </li>
+            <li>
+              <Link to="/game">Game Board</Link>
+            </li>
+            <li>
+              <Link to="/how-to-play">How to Play</Link>
+            </li>
+            <li>
+              <Link to="/pools">Pools</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={NewGame} />
+          <Route path="/game" component={GameBoard} />
+          <Route path="/how-to-play" component={HowToPlay} />
+          <Route path="/pools" component={Pools} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
